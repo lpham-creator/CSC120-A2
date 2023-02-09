0b727428d1217eda02d5448a5e51fa74ac0489cb
Linh Pham <lpham@smith.edu>
2023-02-08 19:50:01 -0500

    Added header comments

--- computer.py
+++ computer.py
@@ -4,8 +4,11 @@ Description: Academic project A1 for the class CSC 120: Object Oriented Programm
      Author: Linh Pham (@lpham-creator)
        Date: 8 Feb 2023
 """
+
+""" The class Computer, which contains a constructor and one method, mimics the attributes of a real computer.\
+"""
 class Computer:
-    
+
     def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, starting_price: int):
         self.description = description
         self.processor_type = processor_type
@@ -15,6 +18,9 @@ class Computer:
         self.year_made = year_made
         self.price = starting_price
     
+    """
+    Returns all the attributes of a computer
+    """
     def getInfo(self):
         return self.description, self.processor_type, self.hard_drive_capacity, self.memory, self.operating_system, self.year_made, self.price
 
